47990fa9aa7a949e2a4a455909e6fa56f1cf0583
James Spence <Jamesaspence20@gmail.com>
2018-01-27 10:42:40 -0600

    renamed App -> Home, test use of react router

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import './Home.css';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <header className="Home-header">
+          <img src={logo} className="Home-logo" alt="logo" />
+          <h1 className="Home-title">Welcome to React</h1>
+        </header>
+        <p className="Home-intro">
+          To get started, edit <code>src/components/Home.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
---
+++ src/components/Test.jsx
@@ -0,0 +1,11 @@
+'use strict';
+
+import React, {Component} from 'react';
+
+export default class Test extends Component {
+  render() {
+    return (
+      <h1>Hello!</h1>
+    );
+  }
+}
