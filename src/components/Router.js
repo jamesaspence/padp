cf913e5424f0d325f1bc44bb5872d58c8c1d88da
James Spence <Jamesaspence20@gmail.com>
2018-01-27 11:04:55 -0600

    removed test routes, unecessary nav

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,27 +1,17 @@
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import React, {Component} from 'react';
 
 /*
  * Route Components
  */
 import Home from './Home';
-import Test from './Test';
 
 export default class Router extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/test">Test</Link>
-            </li>
-          </ul>
           <Route exact path="/" component={Home}/>
-          <Route exact path="/test" component={Test}/>
         </div>
       </BrowserRouter>
     );
