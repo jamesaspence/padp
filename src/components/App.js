16652f95666cc03526940bbb5c6b2a683501f23e
James Spence <Jamesaspence20@gmail.com>
2018-04-08 23:26:04 -0500

    cleaned up code

--- src/components/App.js
+++ src/components/App.js
@@ -24,47 +24,8 @@ export default class App extends Component {
     };
   }
 
-  // componentDidMount() {
-  //   const newState = {
-  //     locationRequested: true
-  //   };
-  //
-  //   if (!('geolocation' in window.navigator)) {
-  //     newState.locationError = true;
-  //     newState.errorMessage = 'Unable to retrieve location - no navigator available.';
-  //     this.setState(newState);
-  //     return;
-  //   }
-  //
-  //   window.navigator.geolocation.getCurrentPosition(position => {
-  //     newState.lat = position.coords.latitude;
-  //     newState.long = position.coords.longitude;
-  //     this.setState(newState);
-  //   }, e => {
-  //     newState.locationError = true;
-  //     newState.errorMessage = e.message;
-  //     this.setState(newState);
-  //   });
-  // }
-
   render() {
-    // let children;
-    //
-    // if (!this.state.locationRequested) {
-    //   children = (
-    //     <div className="column is-half">
-    //       <Loader/>
-    //     </div>
-    //   );
-    // } else if (this.state.locationError) {
-    //   children = <Error errorMessage={this.state.errorMessage}/>;
-    // } else {
-    //   children = [
-    //   ];
-    // }
-
     const renderHome = routeProps => <Home {...routeProps} lat={this.state.lat} long={this.state.long} />;
-    //TODO find way to wrap entirety of app in flex
     return (
       <BrowserRouter>
         <div className="content-root">
