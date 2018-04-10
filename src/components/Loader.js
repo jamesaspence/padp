edc5b26ebb3060504e71dc94862503e7468d451c
James Spence <Jamesaspence20@gmail.com>
2018-04-09 22:43:38 -0500

    removed box shadow via react prop on loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,8 +3,10 @@ import './Loader.css';
 
 export default class Loader extends Component {
   render() {
+    const classNames = 'card is-loading' + (this.props.noBoxShadow ? ' no-shadow' : '');
+
     return (
-      <div className="card is-loading"/>
+      <div className={classNames}/>
     );
   }
 }
