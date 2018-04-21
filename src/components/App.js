4302a640dcce15fa838d3e4b7a696517d45234aa
James Spence <Jamesaspence20@gmail.com>
2018-04-21 10:42:52 -0500

    onLogin prop to google login

--- src/components/App.js
+++ src/components/App.js
@@ -23,17 +23,24 @@ export default class App extends Component {
       lat: null,
       long: null
     };
+
+    this.onLogin = this.onLogin.bind(this);
+  }
+
+  onLogin(response) {
+    console.log(response);
   }
 
   render() {
     const renderHome = routeProps => <Home {...routeProps} lat={this.state.lat} long={this.state.long} />;
+    const renderLogin = routeProps => <Login onLogin={this.onLogin} />;
     return (
       <BrowserRouter>
         <div className="content-root">
           <Header/>
           <Switch>
             <Route key="0" exact path="/home" render={renderHome}/>
-            <Route key="1" exact path="/login" component={Login}/>
+            <Route key="1" exact path="/login" render={renderLogin}/>
             <Route key="2" path="/vote/:sessionId" component={Voter}/>
             <Redirect from="/" to="/login"/>
           </Switch>
