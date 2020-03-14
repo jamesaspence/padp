53134ccab9207e569305298e3814dd20a7274db1
James Spence <Jamesaspence20@gmail.com>
2020-03-14 12:31:30 -0500

    replaced App w/ NewApp

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './react/NewApp';
+import App from './react/App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
