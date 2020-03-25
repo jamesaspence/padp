cc093e7bb34d8e08fdf5c2dacf943bb3c1a2c2a3
James Spence <Jamesaspence20@gmail.com>
2020-03-25 14:28:28 -0500

    WIP - home page initial styling

--- src/react/pages/Home.js
+++ src/react/pages/Home.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import './Home.scss';
 import ContentContainer from '../components/common/ContentContainer';
 import MainContent from '../components/common/MainContent';
+import ContentCard from '../components/common/ContentCard';
+import ContentWrap from '../components/common/ContentWrap';
+import BlockLink from '../components/common/BlockLink';
 
 const Home = () => {
   return (
     <div className="home">
       <ContentContainer>
         <MainContent>
+          <ContentWrap>
+            <ContentCard>
+              <h2 className="title is-2">
+                Start a New Vote
+              </h2>
+              <p className="content">
+                Want to go somewhere? Create a new vote and share the link, so you and your friends can vote!
+              </p>
+              <BlockLink to="/new">NEW VOTE</BlockLink>
+              <h2 className="title is-2">Join a Vote</h2>
+              <p className="content">
+                Been invited to an outing? Enter the provided URL so we can get you set up.
+              </p>
+              <form className="VoteForm">
+                <div className="field">
+                  <label htmlFor="voteUrl" className="label">Vote URL</label>
+                  <div className="control">
+                    <input type="text" id="voteUrl" className="input"/>
+                  </div>
+                </div>
+                <button className="button is-primary is-medium" type="button">Submit</button>
+              </form>
+            </ContentCard>
+          </ContentWrap>
         </MainContent>
       </ContentContainer>
     </div>
